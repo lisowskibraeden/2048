9c95f647264b7548aab6f75d9ed5a409a9e4269e
lisowskibraeden <lisowskibraeden@gmail.com>
2021-08-29 00:22:22 -0500

    add score

--- 2048.py
+++ 2048.py
@@ -18,6 +18,7 @@ seed(datetime.now().timestamp())
 
 newVal = [2, 2, 2, 2, 2, 2, 2, 2, 2, 4]
 colors = [0, '#eeb17d', '#f29866', '#f07f61','#f46042','#eace75', '#edcb67','#ecc85a', '#e5c254','#ecba4e', '#ffae00', '#bbfaa2', '#7bff47', '#48ff00', '#c675ff', '#ac38ff', '#000000']
+score = 0
 class game:
     board = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
     def __init__(self):
@@ -25,8 +26,9 @@ class game:
         game.randomAdd(self)
 
     def restart(self):
-        game.printBoard(self, game.board)
+        game.printBoard(self, game.board) # game restarts too early
         game.board = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
+        score = 0
         game.__init__(self)
         
     def checkMoves(self):
@@ -81,6 +83,7 @@ class game:
             game.checkBoard(self)
         
     def up(self, board):
+        global score
         prev = deepcopy(board)
         for y in range(0, 4):
             # Merge
@@ -91,6 +94,7 @@ class game:
                     if board[y][j] == 0:
                         continue
                     elif board[y][i] == board[y][j]:
+                        score += board[y][i] + board[y][j]
                         board[y][i] = board[y][i] + board[y][j]
                         board[y][j] = 0
                         i = j
@@ -154,27 +158,32 @@ class game:
                 board[len(board)-1-y][x] = temp
 
 def main():
-    print(math.log(65536, 2))
     pygame.init()
     logo = pygame.image.load("logo.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("2048")
-    screen = pygame.display.set_mode([500, 500])
+    screen = pygame.display.set_mode([500, 700])
     pygame.font.init() 
     myfont = pygame.font.SysFont('Arial', 30)
     running = True
     g = game()
     screen.fill((255, 255, 255))
     while running:
+        score_text = myfont.render('Score', False, (0,0,0))
+        score_width, score_height = myfont.size('Score')
+        screen.blit(score_text, (-score_width / 2 + 250, -score_height / 2 + 50))
+        score_num = myfont.render(str(score), False, (0,0,0))
+        score_num_width, score_num_height = myfont.size(str(score))
+        screen.blit(score_num, (-score_num_width / 2 + 250, -score_num_height / 2 + 100))
         for i in range(len(g.board)):
             for x in range(len(g.board)):
                 num = myfont.render(str(g.board[i][x]), False, (0, 0, 0))
                 num_width, num_height = myfont.size(str(g.board[i][x]))
                 if g.board[i][x] != 0:
-                    pygame.draw.rect(screen, colors[int(math.log(g.board[i][x], 2))], pygame.Rect(50 + 500/5 * i, 50 + 500/5 * x, 100, 100), 0, 3)
-                    screen.blit(num, ((-num_width / 2) + 100 + 500/5 * i, (-num_height / 2) + 100 + 500/5 * x))
+                    pygame.draw.rect(screen, colors[int(math.log(g.board[i][x], 2))], pygame.Rect(50 + 500/5 * i, 250 + 500/5 * x, 100, 100), 0, 3)
+                    screen.blit(num, ((-num_width / 2) + 100 + 500/5 * i, (-num_height / 2) + 300 + 500/5 * x))
                 else:
-                    pygame.draw.rect(screen, 'gray', pygame.Rect(50 + 500/5 * i, 50 + 500/5 * x, 100, 100), 0, 3)
+                    pygame.draw.rect(screen, 'gray', pygame.Rect(50 + 500/5 * i, 250 + 500/5 * x, 100, 100), 0, 3)
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_RIGHT:
